04e3ee7e5b2245123b36276f5cdc870a563b30fb
Yuito Murase <yuito@acupof.coffee>
2021-02-07 18:49:34 +0900

    Don't show satysfi output for test run by default

--- test/run-test.sh
+++ test/run-test.sh
@@ -1,7 +1,19 @@
 #! /bin/sh
 
+VERBOSE=0
+while getopts v OPT
+do
+  case $OPT in
+     v) VERBOSE=1;;
+  esac
+done
+
 rm -rf report.txt
-satysfi --text-mode "text" main.test.saty -o report.txt
+if [ $VERBOSE -eq 1 ]; then
+	satysfi --text-mode "text" main.test.saty -o report.txt
+else
+	satysfi --text-mode "text" main.test.saty -o report.txt > /dev/null
+fi
 
 if [ -e report.txt ]; then
 	cat report.txt
